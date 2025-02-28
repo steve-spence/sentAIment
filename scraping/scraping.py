fcd6b3a0347b493b7ac602c03eba8ea6d81717a3
Steven <spenc426@msu.edu>
2025-02-28 00:21:18 -0500

    Fixed my git and updated the files I worked on

--- scraping/scraping.py
+++ scraping/scraping.py
@@ -4,25 +4,32 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import re
+import json
 
-def get_article_percent(print_output=False):
+def get_article_percent(print_output=False) -> dict:
+    """
+    This function scrapes the Yahoo Finance website for the latest news for 
+    titles, stock symbols, and stock value change %
+    :param print_output: If True, will print the title, stock symbol, and stock value change
+    :return: A dictionary of the article title, stock symbol, and stock value change
+    """
+
+    # Set up the Chrome options
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
+
     driver = webdriver.Chrome(options=chrome_options)
     driver.get("https://finance.yahoo.com/news/")
-    driver.implicitly_wait(2)
 
     articleInfo = {}
-
-    ActionChains(driver).scroll_by_amount(0, 800).perform()
 
     story_item = driver.find_elements(By.CLASS_NAME, "story-item")
     for item in story_item:
         try:
             titleElement = item.find_element(By.TAG_NAME, "h3")
 
-            # Wait for the footer psuedo-selectors to be present and visible
+            # Wait for the footer psuedo-element to be present and visible
             footer = WebDriverWait(item, 10).until(
                 EC.presence_of_element_located((By.CLASS_NAME, "footer"))
             )
@@ -41,7 +48,6 @@ def get_article_percent(print_output=False):
             else:
                 #print(f"Couldn't find percent change for: {titleElement.text}")
                 pass
-       
         except Exception:
             if print_output:
                 print("No title found for this story item.")
@@ -51,6 +57,7 @@ def get_article_percent(print_output=False):
         print("No story items found on Yahoo Finance.")
     
     driver.quit()
+    json.dump(articleInfo, open("articleInfo.json", "w"))
     return articleInfo
 
 get_article_percent()
