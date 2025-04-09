a3a5e949a7821f68712940ba963bb7ee5ed0adc8
Steven Spencer <spenc426@msu.edu>
2025-04-08 20:55:25 -0400

    updated files needed for hosting to be successful.

--- Client/next.config.ts
+++ Client/next.config.ts
@@ -1,7 +1,12 @@
+import path from "path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  webpack(config) {
+    config.resolve.alias["@"] = path.resolve(__dirname, "src");
+    return config;
+  },
 };
 
 export default nextConfig;
